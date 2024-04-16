dac9c09124f44223b3358aba23e4227143cdb4ab
franklyn-noyola <franklyn.noyola@gmail.com>
2024-04-16 08:45:46 +0200

    updated

--- XCarAutomation/src/test/java/XCarTest/loginMain.java
+++ XCarAutomation/src/test/java/XCarTest/loginMain.java
@@ -8,7 +8,6 @@ import java.util.ResourceBundle;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import io.appium.java_client.AppiumDriver;
 import appiumtest.settings;
 
